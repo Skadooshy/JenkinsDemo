673af41836806dda7609486508ddeb535a91aa99
Skadooshy <39709537+Skadooshy@users.noreply.github.com>
2021-10-25 13:14:15 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,20 @@
+def envVariables(){
+   echo "Job url: ${JOB_URL}"
+   echo "Commit github: ${GIT_COMMIT}" 
+   echo "This job name is: ${BUILD_DISPLAY_NAME}"
+}
+
 def buildApp(){
   echo 'building application'
+  echo "building version ${NEW_VERSION}"
 }
 
 def testApp(){
   echo 'testing application'
+  echo "Current branch name is ${BRANCH_NAME}" //(localhost:8080/env-vars.html/ for all ENV variables)
 }
 
 def deployApp(){
-  echo 'deploy application'
   echo 'deploying the application....'
   echo "deploying with ${SERVER_CREDENTIALS}"
 }
